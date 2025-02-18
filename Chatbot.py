e0dfd2aca99818156beaf4c1056370ecd951e043
Victoria  Guzman <60210241+Victoriaaguz@users.noreply.github.com>
2025-02-18 14:16:00 -0800

    Responsive reponse

--- Chatbot.py
+++ Chatbot.py
@@ -2,12 +2,22 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 # Create a chatbot instance
-chatbot = ChatBot("TestBot")
+chatbot = ChatBot(
+    "QAbot", 
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    database_uri="sqlite:///db.sqlite3" #stores chat history
+    )
 
 # Train the chatbot
 trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Train on general english conv 
 trainer.train("chatterbot.corpus.english")
 
-# Test response
-response = chatbot.get_response("Hello")
-print("Bot:", response)
+while True: 
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit", "bye"]:
+        print("Bot: Goodbye!")
+        break
+    response = chatbot.get_response(user_input)
+    print("Bot:",response)
